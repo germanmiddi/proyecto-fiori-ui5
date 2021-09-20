74b03b5d7705149db2a06a303c8c71fc97740161
German Middi SAP-BAS <german.middi@gmail.com>
2021-09-20 14:21:34 +0000

    Transalte file i18n

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -9,7 +9,18 @@ sap.ui.define([
 
 		return Controller.extend("logaligroup.invoices.controller.MainView", {
 			onInit: function () {
+                //Se puede simplificar el sap.ui.model.json declarandolo como dependencia
+                const oJSONModel =  new sap.ui.model.json.JSONModel();
+                //Cargo el modelo en el objeto JSON
+                oJSONModel.loadData("./model/SelectionScreenMenu.json")
 
+                // Instancio la vista
+                const oView = this.getView();
+                // Se setea/bindea el modelo en la vista, en el segundo parametro se pasa el nombre que se quiere usar en la vista
+                oView.setModel(oJSONModel, "selectionScreen")
+
+                // Se pueden resumir los pasos de arriba con:
+                // this.getView().setModel(oJSONModel, "selectionScreen")
 			}
 		});
 	});
